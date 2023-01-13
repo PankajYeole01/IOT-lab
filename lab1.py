329c2bec9810f7afeb2a7985d4bcfb1bd9087c29
Pankaj <yeolepd20.extc@coep.ac.in>
2023-01-13 10:26:33 +0530

    Initial Commit

---
+++ lab1.py
@@ -0,0 +1,20 @@
+#Arithmetic operations 
+a=15
+b=3
+print(f"Addition of two numbers is {a+b}")
+print(f"subtraction of two numbers is {a-b}")
+print(f"Multiplication of two numbers is {a*b}")
+print(f"Division of two numbers is {a/b}")
+print(f"Floor division of two numbers is {a//b}")
+print(f"The modulo operation on two numbers is {a%b}")
+#looping statement 
+print("The cube of first ten natural numbers is as follows:")
+x=range(1,10)
+for i in x:
+    print(f"{i} -> + {i*i*i}")
+
+#use of while loop 
+x=10
+while x>0:
+    print(x)
+    x-=1
